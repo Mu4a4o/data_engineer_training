617ca8f677a0c6bace3c7b8e6b57b9a111063221
Mu4a4o <3com@list.ru>
2021-05-28 06:39:57 +0300

    update DZ

--- lesson 4 (python)/DZ.py
+++ lesson 4 (python)/DZ.py
@@ -126,3 +126,77 @@ for key, value in type_dict_rand.items():
     elif True is value:
         count_3 = count_3 + 1
 print(count_1, count_2, count_3)
+
+
+### Оля и Максим
+import  random
+
+"""
+1. Требуется сгенерировать словарь(type_dict_rand) и список(type_list_rand),до 15 элементов в каждой переменной.
+В списке должны быть числа типа str.
+В словаре в качестве ключа должны быть числа типа int,
+а в качестве значений True/False (тип bool и тоже определяются рандомом)
+"""
+type_list_random = [str(random.randrange(0, 20)) for i in range(0, 15)]
+type_dict_random = {random.randrange(0, 20): random.choice([True, False]) for i in range(0, 15)}
+print(type_list_random)
+print(type_dict_random)
+
+
+
+"""
+2. Сделать цикл и условную конструкцию которая ищет одинаковые числа в type_dict_rand и type_list_rand (не забываем про
+преобразование str в int или наоборот при их поиске).В случае совпадений, 
+требуется изменить в type_dict_rand по ключу True на ['False'] и False на 'True', а так-же
+подсчитать кол-во совпадений и вывести после цикла принтом.
+"""
+count = 0
+for key in type_dict_random:
+    for x in type_list_random:
+        if int(x) == key:
+            count += 1
+            if  type_dict_random[key] == False:
+                type_dict_random[key] = 'True'
+            else:
+                type_dict_random[key] = ['False']
+print(count)
+print(type_dict_random)
+
+"""
+3. Сделать следующий цикл, который добавит в type_dict_rand те числа, что не были найдены во 2 пункте ,а так-же
+подсчитать их кол-во  и вывести после цикла принтом.
+Ключ это число типа int, а значением будет True тип bool.
+!!! Подсказка т.к я не я не объяснял вам про "не были найдены",можно вместо is, in, == использовать is not, not in, != 
+"""
+count_2_1 = 0
+for x in type_list_random:
+    if int(x) not in type_dict_random:
+            count_2_1 += 1
+            type_dict_random[int(x)] = True
+print(count_2_1)
+print(type_dict_random)
+
+"""
+4. Подсчитать циклом и  условной конструкцией  сколько в type_dict_rand у нас значений и вывести тремя принтами после цикла :
+    1. ['False'] типа list[str]
+    2. 'True' типа str
+    3. True типа bool
+    4. False типа bool
+"""
+count = [0, 0, 0, 0]
+for key in type_dict_random:
+    if ['False'] == type_dict_random[key]:
+        count[0] += 1
+    elif 'True' == type_dict_random[key]:
+        count[1] += 1
+    elif type_dict_random[key] is True:
+        count[2] += 1
+    else:
+        count[3] += 1
+print(count)
+
+
+
+
+
+
