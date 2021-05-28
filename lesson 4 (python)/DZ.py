ef74cf3e28e2a74b43cb75e9fd05d206baa2ff0b
Mu4a4o <3com@list.ru>
2021-05-28 11:36:04 +0300

    update DZ

--- lesson 4 (python)/DZ.py
+++ lesson 4 (python)/DZ.py
@@ -1,4 +1,5 @@
 import random
+# Денис
 ## 1
 """
 1. Требуется сгенерировать словарь(type_dict_rand) и список(type_list_rand),до 15 элементов в каждой переменной.
@@ -197,24 +198,44 @@ print(count)
 
 
 ###ЧАСТЬ 2 ЗАКРЕПЛЕНИЕ
+# Денис
 """1. Требуется сгенерировать словарь type_dict_rand.Ключами будут рандомные str числа.
 В качестве значений у каждого ключа будет сгенерированный рандомный список состоящи из int чисел.
 Ключей будет 15 шт.
 Значений в каждом списке ключа будет тоже 15 шт.
 Пример {'1': [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],'2':[1,2,3....}
 """
-
+type_dict_rand = {str(random.randrange(0, 15)) : [random.randrange(0, 15) for i in range(0, 15)] for i in range(0, 15)}
+print(type_dict_rand)
+for value in type_dict_rand.values():
+    print(value)
 """
-2.Создать пустой словарь type_dict_count_value. Сделать цикл, где:
+2.Создать пустой словарь type_dict_count_value. Сделать цикл или циклы, где:
 Ключами будут числа, которые есть в значениях списков в словаре type_dict_rand
 А значениями ключей будет кол-во найденых чисел во всем словаре type_dict_rand по всем спискам
 Пример
 Есть словарь type_dict_rand {'5': [1,1,2,10],'1': [5,1,2,100]}
 В итоге в type_dict_count_value должно быть { 1 : 3, 2: 2, 10: 1, 100: 1}
 """
+type_dict_count_value = {}
+for value in type_dict_rand.values():
+    for i in value:
+        if i in type_dict_count_value:
+            type_dict_count_value[i] += 1
+        else:
+            type_dict_count_value[i] = 1
+print(type_dict_count_value)
 
-
-
-
-
-
+"""
+3.Взять ключи из словаря type_dict_rand и сравнить с ключами type_dict_count_value.
+В случае совпадения сделать инкремент значения ключа в type_dict_count_value
+Пример
+type_dict_rand {'5': [1,1,2,10],'1': [5,1,2,100]}
+type_dict_count_value { 1 : 3, 2: 2, 10: 1, 100: 1}
+После цикла будет так
+type_dict_count_value { 1 : 4, 2: 2, 10: 1, 100: 1}
+"""
+for key in type_dict_rand:
+    if int(key) in type_dict_count_value:
+        type_dict_count_value[int(key)] += 1
+print(type_dict_count_value)
