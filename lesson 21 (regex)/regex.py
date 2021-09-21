1929837a8263c6f3e0aecc657216f41c6a9f916e
dgribanov <U6pR7whdywnmMKK>
2021-09-21 09:00:32 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit f5e6c281

--- lesson 21 (regex)/regex.py
+++ lesson 21 (regex)/regex.py
@@ -1,4 +1,4 @@
-/*
+'''
 % - произвольное кол-во символов
 _ - любой одиночный символ
 $ - конец строки
@@ -7,6 +7,7 @@ $ - конец строки
 */
 -- https://regex101.com/
 
+-- Примеры на SQL
 -- Маска это свойства регуляного выражения состоящий из метасимволов и самих символов, которые позволяют обеспечить нужный вывод данных
 -- маска с метасимволом '%' обозначает вывод любых символов произвольного кол-ва
 -- Пример с оператором LIKE, где 'W%' выводит все слова,которые начинаются с WA...
@@ -37,19 +38,73 @@ select distinct first_name from  subscriber_information_big where first_name REG
 select distinct first_name from  subscriber_information_big where first_name REGEXP 'H[OA]T';
 -- Пример поиск подстроки состоящих из символов 'H[A-E]' в указаном порядке, где метасимвол '[-]' обозначает любой символ от A до E в скобках после H
 select distinct first_name from  subscriber_information_big where first_name REGEXP 'H[A-E]';
--- Пример поиск подстроки состоящих из символов 'H[A-E]' в указаном порядке, где метасимвол '[-]' обозначает любой символ от A до E в скобках после H
-select distinct first_name from  subscriber_information_big where first_name REGEXP 'H[A-E]';
+'''
 
--- Далее примеры на https://regex101.com/ где будем использовать остальные метасимволы
-/*
+## Примеры на Pandas
+import pandas as pd
+
+#Сброс ограничений на число столбцов
+pd.set_option('display.max_columns', None)
+'''
+https://habr.com/ru/post/349860/
 \ - экранирование
 \d - любую цифру
 \D - все что угодно, кроме цифр
 \s - пробелы
 \S - все кроме пробелов
-\w - буква
-\W - все кроме букв
+\w - буква или цифра
+\W - все кроме букв или цифр
 \b - граница слова
 \B - не границ
-*/
-select id_abon from subscriber_information_big;
+{2}- повторение два раза
+'''
+
+path_file = '/Users/dgribanov/PycharmProjects/data_engineer_training/lesson 20 (pandas,generation)/big_si.csv'
+# открываем файл с явными типами
+df_si = pd.read_csv(path_file,
+                    sep=';',
+                    header=None,
+                    dtype=
+                            {
+                                0: 'object',
+                                1: 'category',
+                                2: 'category',
+                                4: 'int8',
+                                5: 'int8',
+                                6: 'int8',
+                                7: 'category',
+                            })
+
+print(df_si.info(memory_usage='deep'))
+
+name_columns = ['id_abon', 'first_name', 'last_name', 'connection_date', 'trust_payment', 'number_of_internet_devices',
+                'number_of_tv_devices', 'comment_when_сonnecting']
+df_si.columns = name_columns
+
+# Форматируем в дату
+df_si['connection_date'] = pd.to_datetime(df_si['connection_date'], format='%Y-%m-%d')
+print(df_si.info(memory_usage='deep'))
+
+# В отличии от SQL чувстителен к регистру !!!
+
+##Пример поиск подстроки состоящих из символов 'W|S' в указаном порядке, где метасимвол '|' обозначает 'или'
+df_si[df_si['first_name'].str.contains('[W|S]', regex= True, na=False)]
+
+## Пример поиск подстроки состоящих из символов '^Wi' в указаном порядке, где метасимвол '^' (циркуфлекс,крышка) обозначает начало строки
+df_si[df_si['first_name'].str.contains('^Wi', regex= True, na=False)]
+
+## Пример поиск подстроки состоящих из символов 'at$' в указаном порядке, где метасимвол '$' обозначает конец строки
+df_si[df_si['first_name'].str.contains('at$', regex= True, na=False)]
+
+## Пример поиск подстроки состоящих из символов '\d\d\d\d\d\d\D\D-\d' в указаном порядке,
+# где метасимвол '\d' обозначает любая цифра,
+# а метасимвол '\D' обозначает любая НЕ цифра,
+df_si[df_si['id_abon'].str.contains('\d\d\d\d\d\d\D\D-\d', regex= True, na=False)]
+
+## Пример поиск подстроки состоящих из символов '\W\d\d[a-zA-Z]' в указаном порядке,
+# где метасимвол '\W' обозначает все кроме букв или цифр,
+# '\d' обозначает любая цифра,
+# '[a-zA-Z]' обозначает любая буква,
+# '{2}' повторение два раза
+df_si[df_si['id_abon'].str.contains('\W\d\d[a-zA-Z]\d\W', regex= True, na=False)]
+#df_si[df_si['id_abon'].str.contains('\W\d{2}[a-zA-Z]\d\W', regex= True, na=False)]
