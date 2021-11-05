cb09d684c2b30ccf8eef7e3c43b110c43df9e455
Mu4a4o <3com@list.ru>
2021-11-05 10:04:50 +0300

    1

--- lesson 9 (Mysql,docker)/create.sql
+++ lesson 9 (Mysql,docker)/create.sql
@@ -26,7 +26,15 @@ docker stop mysql_instance
 docker start mysql_instance
 docker run --name=mysql_instance -p 3305:3306 mysql
 
-
+-- сохранить образ контейенера и переместить
+1. запускаем на контейнер
+2. docker ps (копируем CONTAINER ID)
+3. docker commit "CONTAINER ID" "NAME IMAGE"
+(коммитим в образ и не забываем вставить "CONTAINER ID" из 2 пункта + "NAME IMAGE" название образа)
+4. docker save "NAME IMAGE" > "name_file.tar" (сохраняем файл образа)
+5. "name_file.tar" (файл образа) переносим на другой сервер
+6. docker load < "name_file.tar" (загружаем образ в другой докер)
+7. docker run --name mysql_instance -e MYSQL_ROOT_PASSWORD=mu4a4o212 -e LANG=C.UTF-8 -p 3308:3306 -d "NAME IMAGE"
 
 
 
